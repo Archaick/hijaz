80e7374d8115a909042c5c9aa4be461d5d87648d
Saeed <saeed.jh3@gmail.com>
2026-04-09 15:02:14 +0700

    fix firebase deploy file track

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.js');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+};
 
 export default withNextIntl(nextConfig);
